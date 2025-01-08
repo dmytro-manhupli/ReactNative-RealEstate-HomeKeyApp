beb3ca6f53d5bff8e8866a6e0669c99f628b0d23
dmytro-manhupli <dmytro.manhupli@gmail.com>
2025-01-09 03:51:38 +0800

    chore: security

--- src/env.js
+++ src/env.js
@@ -10,9 +10,9 @@ if (__DEV__) {
     ...defaults,
     // API_URL: 'http://re.dev/api',
     API_URL: 'http://sood.ideasowners.net/api',
-    GOOGLE_MAPS_KEY: 'AIzaSyCpQX4H0QPxVgKuNMZ0ELG_ymgT8RHcKh4',
-    GOOGLE_MAPS_IOS_KEY: 'AIzaSyDPCgdWqrkBe4v3uSuU-MZGJIZ0AQxfbCo',
-    GOOGLE_MAPS_ANDROID_KEY: 'AIzaSyCpQX4H0QPxVgKuNMZ0ELG_ymgT8RHcKh4',
+    GOOGLE_MAPS_KEY: '',
+    GOOGLE_MAPS_IOS_KEY: '',
+    GOOGLE_MAPS_ANDROID_KEY: '',
     CODEPUSH_ENABLED: false,
   };
   // XMLHttpRequest = GLOBAL.originalXMLHttpRequest
@@ -22,9 +22,9 @@ if (__DEV__) {
   module.exports = {
     ...defaults,
     API_URL: 'http://sood.ideasowners.net/api',
-    GOOGLE_MAPS_KEY: 'AIzaSyCpQX4H0QPxVgKuNMZ0ELG_ymgT8RHcKh4',
-    GOOGLE_MAPS_IOS_KEY: 'AIzaSyDPCgdWqrkBe4v3uSuU-MZGJIZ0AQxfbCo',
-    GOOGLE_MAPS_ANDROID_KEY: 'AIzaSyCpQX4H0QPxVgKuNMZ0ELG_ymgT8RHcKh4',
+    GOOGLE_MAPS_KEY: '',
+    GOOGLE_MAPS_IOS_KEY: '',
+    GOOGLE_MAPS_ANDROID_KEY: '',
     CODEPUSH_ENABLED: true,
   };
 }
